5fc79e332f853ae4a51b4ac970cc3978d386f82b
ngeloanciotto <aangeloppanciotto@gmail.com>
2025-08-22 22:50:42 +0200

    Added CLI argument functionality for user input

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from stats import count_chars, count_words, sort_dictionaries
+import sys
 
 def main():
-    path_to_book = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path_to_book = sys.argv[1] #the second argument of sys.argv is the path to the book
     text = get_book_text(path_to_book)
     num_chars = count_chars(text)
     num_words = count_words(text)

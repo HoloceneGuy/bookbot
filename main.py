54245b450ff5e2b2a58bc5dcb2bb03223fe972b6
ngeloanciotto <aangeloppanciotto@gmail.com>
2025-08-22 22:30:11 +0200

    Working tool before argument input mode

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 from stats import count_chars, count_words, sort_dictionaries
 
-def get_book_text(path_to_file):
-    with open(path_to_file) as f:
-        file_contents = f.read()
-    return file_contents
-
 def main():
-   
-    num_chars = count_chars("books/frankenstein.txt")
-    #print(num_chars)
-    
-    num_words = count_words("books/frankenstein.txt")
-    #print(f"{num_words} words found in the document")
-
+    path_to_book = "books/frankenstein.txt"
+    text = get_book_text(path_to_book)
+    num_chars = count_chars(text)
+    num_words = count_words(text)
     sorted_dict = sort_dictionaries(num_chars)
-    print(sorted_dict)
+    print_report(path_to_book, num_chars, num_words, sorted_dict)
 
-    #book = get_book_text("books/frankenstein.txt")
-    #print(book)
+def get_book_text(path_to_file):
+    with open(path_to_file) as b:
+        book_contents = b.read()
+    return book_contents
+
+def print_report(path_to_book, num_chars, num_words, sorted_dict):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_to_book}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for listed_dictionary in sorted_dict:
+        if listed_dictionary["char"].isalpha():
+            print(f"{listed_dictionary["char"]}: {listed_dictionary["num"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,34 +1,45 @@
 import string
 
-def count_words(path_to_file):
-    with open(path_to_file) as b:
-        book_contents = b.read()
-        word_list = book_contents.split()
-        num_words = len(word_list)
+def count_words(text):
+    char_list = text.split()
+    num_words = len(char_list)
     return num_words
 
-def count_chars(path_to_file):
+def count_chars(text):
     char_dict = {}
 
-    with open(path_to_file) as book:
-        book_contents = book.read()
-        book_chars = list(book_contents.lower())
-        checking_chars = list(string.ascii_lowercase + " ")
-        for character in book_chars:
-            if character in checking_chars:
-                char_dict.setdefault(character, 0)
-                char_dict[character] += 1
-
+    for character in text:
+        lowered = character.lower() #
+        if lowered in char_dict:
+            char_dict[lowered] += 1
+        else:
+            char_dict[lowered] = 1
     return char_dict
+
+### Old way still using a raw path to the book and converting to the text within the function
+#def count_chars(path_to_file):
+#    char_dict = {}
+#
+#    with open(path_to_file) as book:
+#        book_contents = book.read()
+#        book_chars = list(book_contents.lower())
+#        checking_chars = list(string.ascii_lowercase + " ")
+#        for character in book_chars:
+#            if character in checking_chars:
+#                char_dict.setdefault(character, 0)
+#                char_dict[character] += 1
+#
+#    return char_dict
+
+def sort_on(items):
+    return items["num"]
 
 def sort_dictionaries(char_dict):
     listed_char_dicts = []
     for key in char_dict:
         counted_character = {"char":key, "num":char_dict[key]}
         listed_char_dicts.append(counted_character)
+    
+    listed_char_dicts.sort(reverse=True, key=sort_on)
 
     return listed_char_dicts
-
-#def sort_on(items):
-#    return items["num"]
-
